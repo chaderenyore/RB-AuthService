1212f7e80f33b74f32d14cc3e565f1397930d3ac
Adjaro Ogaga <ogagaadjaro@gmail.com>
2023-01-25 12:41:54 +0100

    Chore-Added Deployment Scripts

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const app = require("./src");
 const KEYS = require("./src/_config/keys");
 const logger  = require('./logger.conf');
-// const { createQConnection } = require("./src/config/queue/_init_queue");
 
 
 mongoose.set('strictQuery', true);
@@ -12,10 +11,11 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => {
+    logger.info(`Auth Service Database Connected......`)
     const PORT = process.env.PORT || 2100;
     const server = app.listen(PORT, () => {
       
-      logger.info(`Server has started!... and running on port ${PORT}`);
+      logger.info(`Auth Service Server has started!... and running on port ${PORT}`);
       
     });
   }).catch(error => console.log(error));
